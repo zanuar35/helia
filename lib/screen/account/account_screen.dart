041221bf5e936d816a68e5cfc92dd1d3d0434ec8
zanuar35 <rahmatzanuar35@gmail.com>
2022-08-14 21:31:08 +0700

    fix layout screen

--- lib/screen/account/account_screen.dart
+++ lib/screen/account/account_screen.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: void_checks
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:helia/core/app_colors.dart';
+import 'package:helia/screen/edit_profile/edit_profile.dart';
+import 'package:helia/screen/notification/notification.dart';
+import 'package:helia/screen/security/security.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
@@ -16,13 +21,16 @@ class _AccountScreenState extends State<AccountScreen> {
       appBar: AppBar(
         backgroundColor: AppColors.white,
         elevation: 0,
-        title: const Text('Profile',
-            style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                letterSpacing: .5)),
+        title: const Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: Text('Profile',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: .5)),
+        ),
         leading: Padding(
-          padding: const EdgeInsets.all(5.0),
+          padding: const EdgeInsets.only(top: 15, left: 15),
           child: Container(
             height: 50,
             width: 50,
@@ -32,6 +40,14 @@ class _AccountScreenState extends State<AccountScreen> {
             ),
           ),
         ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_horiz_outlined,
+                color: Colors.red,
+              )),
+        ],
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -41,8 +57,7 @@ class _AccountScreenState extends State<AccountScreen> {
           padding: const EdgeInsets.symmetric(vertical: 16),
           width: double.infinity,
           height: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: ListView(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -61,6 +76,7 @@ class _AccountScreenState extends State<AccountScreen> {
                 height: 10,
               ),
               const Text("John Doe",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
@@ -69,35 +85,67 @@ class _AccountScreenState extends State<AccountScreen> {
                 height: 5,
               ),
               const Text("Email@gmail.com",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                       color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: .5,
-                      fontSize: 20)),
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: .7,
+                      fontSize: 13)),
               const SizedBox(
-                height: 30,
+                height: 10,
               ),
               rowWidget(
                   text: "Edit Profile",
                   iconData: FontAwesomeIcons.solidUser,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const EditProfile()));
+                  },
                   color: Colors.grey),
               rowWidget(
                   text: "Payment",
                   iconData: FontAwesomeIcons.wallet,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const EditProfile()));
+                  },
                   color: Colors.grey),
               rowWidget(
-                  text: "Notofication",
+                  text: "Notification",
                   iconData: FontAwesomeIcons.solidBell,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const NotificationPage()));
+                  },
                   color: Colors.grey),
               rowWidget(
                   text: "Security",
                   iconData: FontAwesomeIcons.shield,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SecurityScreen()));
+                  },
                   color: Colors.grey),
               rowWidget(
                   text: "help",
                   iconData: FontAwesomeIcons.circleInfo,
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const EditProfile()));
+                  },
                   color: Colors.grey),
               rowWidget(
+                  onTap: logOut,
                   text: "Logout",
                   iconData: FontAwesomeIcons.arrowRightFromBracket,
                   color: Colors.red)
@@ -111,35 +159,112 @@ class _AccountScreenState extends State<AccountScreen> {
   Widget rowWidget(
       {required String text,
       required IconData iconData,
+      required GestureTapCallback onTap,
       required Color color}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        children: [
-          Expanded(
-              child: SizedBox(
-            height: 60,
-            child: Row(
-              children: [
-                FaIcon(
-                  iconData,
-                  color: color,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Text(
-                  text,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey,
-                      letterSpacing: .5),
-                )
-              ],
-            ),
-          ))
-        ],
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 2,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+                child: SizedBox(
+              height: 55,
+              child: Row(
+                children: [
+                  FaIcon(
+                    iconData,
+                    color: color,
+                    size: 20,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    text,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey,
+                        letterSpacing: .5),
+                  )
+                ],
+              ),
+            ))
+          ],
+        ),
       ),
+    );
+  }
+
+  void logOut() {
+    showModalBottomSheet<void>(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          padding: const EdgeInsets.only(top: 20),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              const Text(
+                'Logout',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red),
+              ),
+              const Text(
+                'Are you sure you want to logout?',
+                style: TextStyle(
+                    fontSize: 16,
+                    letterSpacing: .4,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(const Size(300, 45)),
+                  backgroundColor: MaterialStateProperty.all(AppColors.green),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                child: const Text('Yes'),
+                onPressed: () => Navigator.pop(context),
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(const Size(300, 45)),
+                  backgroundColor:
+                      MaterialStateProperty.all(const Color(0xffE8F8EF)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                child: Text(
+                  'Cancel',
+                  style: TextStyle(color: AppColors.green),
+                ),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
